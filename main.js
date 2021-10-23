395d9049b7fd4ea2711db1108c1286fbd98a6688
WdvOps <dvops@outlook.com>
2021-10-23 15:50:21 -0300

    Finalized project

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const socialMediasLinks = {
+    github: 'WdvOps',
+    linkedin: 'will-freittas',
+    instagram: 'afreittaswill',
+    facebook: 'william.freittas.54'
+};
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class');
+
+        li.children[0].href = `https://${social}.com/${socialMediasLinks[social]}`
+
+    }
+}
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${socialMediasLinks.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name,
+                bioProfile.textContent = data.bio,
+                userLink.href = data.html_url,
+                userPhoto.src = data.avatar_url,
+                userLogin.textContent = data.login
+        })
+}
+getGitHubProfileInfos()
